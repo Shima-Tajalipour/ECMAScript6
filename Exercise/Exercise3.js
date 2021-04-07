5baf1516b2f324a2702262d808bba38d68e96906
Shima.Tajalipour <shima.tajalipour@gmail.com>
2021-04-07 17:19:54 +0200

    I add exercises of ES6

---
+++ Exercise/Exercise3.js
@@ -0,0 +1,11 @@
+//Exercise 3
+/*
+let phrase = "Hello"
+if (true){    //always true
+    let user="John";
+    function sayHi() {
+        alert(`${phrase},${user}`)
+    }
+}
+sayHi(); //Hello,John
+*/
---
+++ Exercise/Exercise5.js
@@ -0,0 +1,9 @@
+//Exercise 5
+/*
+const user={
+    name:"John"
+}
+user.name = "pete";
+console.log(user)
+//because the object is const not the keys and we are changing the value of key not the object
+*/
