d38f9bf738ff0dfe48bcbecfb4726da258b67f96
Shima.Tajalipour <shima.tajalipour@gmail.com>
2021-04-09 01:16:54 +0200

    I add ECMAScript-exercises

---
+++ ECMAScript-exercises/Exercise4.js
@@ -0,0 +1,10 @@
+//Exercise 4
+/*
+let user={};
+user.name="John";
+user.surname="Smith";
+user.name="Pete";
+console.log(user);
+delete user.name;
+console.log(user);
+*/
---
+++ ECMAScript-exercises/Exercise8.js
@@ -0,0 +1,9 @@
+//Exercise 8
+let message;
+let login;
+login ="Director"
+let result=(login == "Employee")? message="Hello":
+(login == "Director")? message="Greeting":
+(login == "")? message ="No login":
+message="no";
+console.log(message);
